bca51be2e7c2881ac14dd6af3af6cb16a5cf80fe
TosiaDan <antonina.ivanets@gmail.com>
2022-10-16 17:52:32 +0200

    adds last changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -13,11 +13,11 @@ imageContainer.insertAdjacentHTML('beforeend', imageMarkup);
 function createGalleryMarkup(images) {
   return images
     .map(({ preview, original, description }) => {
-      return `    <li class="gallery__item">
+      return `    <div class="gallery__item">
                     <a class="gallery__link" href="${original}">
                         <img class="gallery__image" src="${preview}" alt="${description}" />
                     </a>
-                  </li>
+                  </div>
                `;
     })
     .join('');
