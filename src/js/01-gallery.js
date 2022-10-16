805bf307bd526750885ac23a63748d2bf45e0776
TosiaDan <antonina.ivanets@gmail.com>
2022-10-16 17:37:13 +0200

    adds from desktop

---
+++ src/js/01-gallery.js
@@ -0,0 +1,31 @@
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+const imageContainer = document.querySelector('.gallery');
+const imageMarkup = createGalleryMarkup(galleryItems);
+
+imageContainer.insertAdjacentHTML('beforeend', imageMarkup);
+
+function createGalleryMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `    <li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img class="gallery__image" src="${preview}" alt="${description}" />
+                    </a>
+                  </li>
+               `;
+    })
+    .join('');
+}
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
