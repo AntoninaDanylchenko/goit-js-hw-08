2869aaa473d26a5c3f2262ec1a47ea00cfd0402e
TosiaDan <antonina.ivanets@gmail.com>
2022-10-16 17:48:05 +0200

    delete console.log

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,6 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-
 player.on('timeupdate', throttle(saveCurrentTime, 1000));
 
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
