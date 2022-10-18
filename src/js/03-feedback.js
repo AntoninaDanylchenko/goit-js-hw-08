8949bdfa388811ceb7efa380ae7dcd56aac2dc62
TosiaDan <antonina.ivanets@gmail.com>
2022-10-18 21:22:47 +0200

    corrects 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,6 @@
 import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
-const inputEl = document.querySelector('input');
-const textAreaEl = document.querySelector('textarea');
 
 const formValueToLocal = {};
 messageFromLocal();
@@ -24,17 +22,14 @@ function onInputElements(e) {
   formValueToLocal[e.target.name] = e.target.value;
 
   localStorage.setItem('feedback-form-state', JSON.stringify(formValueToLocal));
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
 }
 
 function messageFromLocal() {
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-  let savedMessage = localStorage.getItem('feedback-form-state');
+  const savedMessage = localStorage.getItem('feedback-form-state');
 
   if (savedMessage) {
     const parsedMessage = JSON.parse(savedMessage);
 
-    console.log(parsedMessage);
     Object.entries(parsedMessage).forEach(([name, value]) => {
       formValueToLocal[name] = value;
       formEl.elements[name].value = value;
