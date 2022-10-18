7ae6b957ff7b7b91b8da39c4957bd9377e8e851f
TosiaDan <antonina.ivanets@gmail.com>
2022-10-18 22:03:35 +0200

    replaces local variablesЄ

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,8 @@
 import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
-
-const formValueToLocal = {};
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+// const formValueToLocal = {};
 messageFromLocal();
 
 formEl.addEventListener('input', throttle(onInputElements, 1000));
@@ -11,27 +11,30 @@ formEl.addEventListener('submit', onFormSubmit);
 function onFormSubmit(event) {
   event.preventDefault();
 
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
 
   event.currentTarget.reset();
 
-  localStorage.removeItem('feedback-form-state');
+  localStorage.removeItem(LOCALSTORAGE_KEY);
 }
 
 function onInputElements(e) {
-  formValueToLocal[e.target.name] = e.target.value;
+  // formValueToLocal[e.target.name] = e.target.value;
+  let someDatasFromLocal = localStorage.getItem(LOCALSTORAGE_KEY);
 
-  localStorage.setItem('feedback-form-state', JSON.stringify(formValueToLocal));
+  someDatasFromLocal = someDatasFromLocal ? JSON.parse(someDatasFromLocal) : {};
+
+  someDatasFromLocal[e.target.name] = e.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(someDatasFromLocal));
 }
 
 function messageFromLocal() {
-  const savedMessage = localStorage.getItem('feedback-form-state');
+  const savedMessage = localStorage.getItem(LOCALSTORAGE_KEY);
 
   if (savedMessage) {
     const parsedMessage = JSON.parse(savedMessage);
 
     Object.entries(parsedMessage).forEach(([name, value]) => {
-      formValueToLocal[name] = value;
       formEl.elements[name].value = value;
     });
   }
